b9e80ddce59349e98d43d5df1f8dfdb256a1f2dd
TynaRan <3314969485@qq.com>
2025-04-29 21:31:53 +0800

    Update extreme fixed.lua

--- extreme fixed.lua
+++ extreme fixed.lua
@@ -187,3 +187,118 @@ coroutine.wrap(function()
         end
     end
 end)()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+
+---====== Timer-based Execution ======---
+
+coroutine.wrap(function()
+    task.wait(135) -- Wait for 135 seconds before executing entity logic
+
+    ---====== Create entity ======---
+
+    local entity = spawner.Create({
+        Entity = {
+            Name = "Obsession",
+            Asset = "https://github.com/Brololto/ExtremeModeG-95/blob/main/G-95Remastered-1.rbxm?raw=true",
+            HeightOffset = 0
+        },
+        Lights = {
+            Flicker = {
+                Enabled = true,
+                Duration = 1
+            },
+            Shatter = true,
+            Repair = false
+        },
+        Earthquake = {
+            Enabled = true
+        },
+        CameraShake = {
+            Enabled = true,
+            Range = 100,
+            Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
+        },
+        Movement = {
+            Speed = 100,
+            Delay = 2,
+            Reversed = false
+        },
+        Rebounding = {
+            Enabled = false,
+            Type = "Ambush",
+            Min = 1,
+            Max = 1,
+            Delay = 0
+        },
+        Damage = {
+            Enabled = true,
+            Range = 100,
+            Amount = 125
+        },
+        Crucifixion = {
+            Enabled = true,
+            Range = 40,
+            Resist = false,
+            Break = true
+        },
+        Death = {
+            Type = "Curious",
+            Hints = {"Oh hi", "I'm light", ":))) I WILL KILL YOU", "Scream!!!"},
+            Cause = "G-95"
+        }
+    })
+
+    ---====== Debug entity ======---
+
+    entity:SetCallback("OnSpawned", function()
+        applyTween(Color3.fromRGB(255, 0, 0), 1.5) -- Red color effect for 1.5 seconds
+    end)
+
+    entity:SetCallback("OnStartMoving", function()
+        print("Entity has started moving")
+    end)
+
+    entity:SetCallback("OnEnterRoom", function(room, firstTime)
+        if firstTime == true then
+            print("Entity has entered room: ".. room.Name.. " for the first time")
+        else
+            print("Entity has entered room: ".. room.Name.. " again")
+        end
+    end)
+
+    entity:SetCallback("OnLookAt", function(lineOfSight)
+        if lineOfSight == true then
+            print("Player is looking at entity")
+        else
+            print("Player view is obstructed by something")
+        end
+    end)
+
+    entity:SetCallback("OnRebounding", function(startOfRebound)
+        if startOfRebound == true then
+            print("Entity has started rebounding")
+        else
+            print("Entity has finished rebounding")
+        end
+    end)
+
+    entity:SetCallback("OnDespawning", function()
+        print("Entity is despawning")
+    end)
+
+    entity:SetCallback("OnDespawned", function()
+        print("Entity has despawned")
+    end)
+
+    entity:SetCallback("OnDamagePlayer", function(newHealth)
+        if newHealth == 0 then
+            print("Entity has killed the player")
+        else
+            print("Entity has damaged the player")
+        end
+    end)
+
+    ---====== Run entity ======---
+
+    entity:Run()
+end)()
