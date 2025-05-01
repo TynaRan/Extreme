2d0c2869eb5047e2d9eb8e44c43db0d307f7034a
TynaRan <3314969485@qq.com>
2025-05-01 15:52:10 +0800

    Update extreme mode.lua

--- extreme mode.lua
+++ extreme mode.lua
@@ -128,71 +128,84 @@ local entityName = "Void Stalker"
 
 coroutine.wrap(function()
     while task.wait(65) do
-        local Clock = GetGitSound(
-            "https://github.com/Brololto/bing-/blob/main/Screen_Recording_20230419-164136_Chrome%20(online-audio-converter.com).mp3?raw=true",
-            entityName .. " Clock"
-        )
-        Clock.Parent = workspace
-        Clock.Volume = 5
-        Clock:Play()
+        local latestRoom = game.ReplicatedStorage.GameData.LatestRoom.Value
+        local seekMovingExists = false
 
-        wait(1)
+        for _, obj in ipairs(game.Workspace:GetChildren()) do
+            if string.find(obj.Name, "SeekMoving") then
+                seekMovingExists = true
+                break
+            end
+        end
 
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 9
+        if latestRoom == 50 or latestRoom == 100 or seekMovingExists then
+            return -- Stops execution if room is 50/100 or any object contains "SeekMoving" in its name
+        else
+            local Clock = GetGitSound(
+                "https://github.com/Brololto/bing-/blob/main/Screen_Recording_20230419-164136_Chrome%20(online-audio-converter.com).mp3?raw=true",
+                entityName .. " Clock"
+            )
+            Clock.Parent = workspace
+            Clock.Volume = 5
+            Clock:Play()
 
-        local Bell = GetGitSound(
-            "https://github.com/Potato032/Entt/blob/main/ClockSoundsCreditsToVynixus.mp3?raw=true",
-            entityName .. " Bell"
-        )
-        Bell.Parent = workspace
-        Bell.Volume = 5
-        Bell:Play()
+            wait(1)
 
-        game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(71, 151, 211)
-        game.Lighting.MainColorCorrection.Contrast = 1
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 9
 
-        local tween = game:GetService("TweenService")
-        tween:Create(game.Lighting.MainColorCorrection, TweenInfo.new(2.5), {Contrast = 0}):Play()
+            local Bell = GetGitSound(
+                "https://github.com/Potato032/Entt/blob/main/ClockSoundsCreditsToVynixus.mp3?raw=true",
+                entityName .. " Bell"
+            )
+            Bell.Parent = workspace
+            Bell.Volume = 5
+            Bell:Play()
 
-        local TweenService = game:GetService("TweenService")
-        local TW = TweenService:Create(game.Lighting.MainColorCorrection, TweenInfo.new(80), {TintColor = Color3.fromRGB(255, 255, 255)})
-        TW:Play()
+            game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(71, 151, 211)
+            game.Lighting.MainColorCorrection.Contrast = 1
 
-        wait(34)
+            local tween = game:GetService("TweenService")
+            tween:Create(game.Lighting.MainColorCorrection, TweenInfo.new(2.5), {Contrast = 0}):Play()
 
-        local character = game.Players.LocalPlayer.Character
-        local humanoid = character and character:FindFirstChild("Humanoid")
-        local startCFrame = character and character.PrimaryPart.CFrame
+            local TweenService = game:GetService("TweenService")
+            local TW = TweenService:Create(game.Lighting.MainColorCorrection, TweenInfo.new(80), {TintColor = Color3.fromRGB(255, 255, 255)})
+            TW:Play()
 
-        if humanoid and startCFrame then
-            wait(math.random(1, 3))
+            wait(34)
 
-            local moved = false
-            local function checkMovement()
-                for _ = 1, 10 do -- 2.5 seconds check (0.1 * 10 = 1 second)
-                    task.wait(0.1)
-                    if humanoid.MoveDirection.Magnitude > 0 then
-                        moved = true
+            local character = game.Players.LocalPlayer.Character
+            local humanoid = character and character:FindFirstChild("Humanoid")
+            local startCFrame = character and character.PrimaryPart.CFrame
+
+            if humanoid and startCFrame then
+                wait(math.random(1, 3))
+
+                local moved = false
+                local function checkMovement()
+                    for _ = 1, 10 do
+                        task.wait(0.1)
+                        if humanoid.MoveDirection.Magnitude > 0 then
+                            moved = true
+                        end
                     end
                 end
-            end
 
-            coroutine.wrap(checkMovement)()
+                coroutine.wrap(checkMovement)()
 
-            task.wait(2.5)
+                task.wait(2.5)
 
-            if moved then
-                humanoid.Health = 0
-                EntityInfo.DeathHint(entityName .. " is watching... You should not have moved...", "Blue")
-                task.wait(math.random(1, 2))
-                EntityInfo.DeathHint(entityName .. " whispers... Your end has come...", "Blue")
-                task.wait(math.random(2, 3))
-                EntityInfo.DeathHint(entityName .. " fades away... Farewell...", "Blue")
-            else
-                -- Restore world settings after all checks are complete
-                game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(255, 255, 255)
-                game.Lighting.MainColorCorrection.Contrast = 0
-                game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+                if moved then
+                    humanoid.Health = 0
+                    EntityInfo.DeathHint(entityName .. " is watching... You should not have moved...", "Blue")
+                    task.wait(math.random(1, 2))
+                    EntityInfo.DeathHint(entityName .. " whispers... Your end has come...", "Blue")
+                    task.wait(math.random(2, 3))
+                    EntityInfo.DeathHint(entityName .. " fades away... Farewell...", "Blue")
+                else
+                    game.Lighting.MainColorCorrection.TintColor = Color3.fromRGB(255, 255, 255)
+                    game.Lighting.MainColorCorrection.Contrast = 0
+                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+                end
             end
         end
     end
@@ -200,158 +213,184 @@ end)()
 local entityName = "disappointed"
 
 coroutine.wrap(function()
-    local startTime = tick() -- Record the starting time
+    while task.wait(85) do
+        local latestRoom = game.ReplicatedStorage.GameData.LatestRoom.Value
+        local seekMovingExists = false
 
-    while task.wait(85) do -- Loop until 50 seconds have passed
-        local static = Instance.new("Sound")
-        static.SoundId = "rbxassetid://9120425687"
-        static.Parent = game.ReplicatedStorage
-        static.Name = "Se"
-        static.Pitch = 0.6
-        static.Volume = 1.5
-        static.TimePosition = 0.2
-        static:Play()
-
-        local GUI = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
-        local Image = Instance.new("ImageLabel", GUI)
-
-        GUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-        Image.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        Image.BackgroundTransparency = 1
-        Image.Size = UDim2.new(1, 0, 1, 0)
-        Image.Image = "rbxassetid://13362464118"
-        Image.ImageTransparency = 0
-
-        task.wait(1)
-        game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
-
-        local character = game.Players.LocalPlayer.Character
-        local humanoid = character and character:FindFirstChild("Humanoid")
-        local startCFrame = character and character.PrimaryPart.CFrame
-
-        if humanoid then
-            local moved = false
-            for _ = 1, 10 do
-                task.wait(0.1)
-                if humanoid.MoveDirection.Magnitude > 0 then
-                    moved = true
-		end
-            end
-
-            if moved then
-                humanoid.Health = 0
-                EntityInfo.DeathHint(entityName .. " is watching... You should not have moved...", "Blue")
-                task.wait(math.random(1, 2))
-                EntityInfo.DeathHint(entityName .. " whispers... Your end has come...", "Blue")
-                task.wait(math.random(2, 3))
-                EntityInfo.DeathHint(entityName .. " fades away... Farewell...", "Blue")
+        for _, obj in ipairs(game.Workspace:GetChildren()) do
+            if string.find(obj.Name, "SeekMoving") then
+                seekMovingExists = true
                 break
             end
         end
+
+        if latestRoom == 50 or latestRoom == 100 or seekMovingExists then
+            return -- Stops execution if room is 50/100 or any object contains "SeekMoving" in its name
+        else
+            local static = Instance.new("Sound")
+            static.SoundId = "rbxassetid://9120425687"
+            static.Parent = game.ReplicatedStorage
+            static.Name = "Se"
+            static.Pitch = 0.6
+            static.Volume = 1.5
+            static.TimePosition = 0.2
+            static:Play()
+
+            local GUI = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+            local Image = Instance.new("ImageLabel", GUI)
+
+            GUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+            Image.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            Image.BackgroundTransparency = 1
+            Image.Size = UDim2.new(1, 0, 1, 0)
+            Image.Image = "rbxassetid://13362464118"
+            Image.ImageTransparency = 0
+
+            task.wait(1)
+            game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
+
+            local character = game.Players.LocalPlayer.Character
+            local humanoid = character and character:FindFirstChild("Humanoid")
+            local startCFrame = character and character.PrimaryPart.CFrame
+
+            if humanoid then
+                local moved = false
+                for _ = 1, 10 do
+                    task.wait(0.1)
+                    if humanoid.MoveDirection.Magnitude > 0 then
+                        moved = true
+                    end
+                end
+
+                if moved then
+                    humanoid.Health = 0
+                    EntityInfo.DeathHint(entityName .. " is watching... You should not have moved...", "Blue")
+                    task.wait(math.random(1, 2))
+                    EntityInfo.DeathHint(entityName .. " whispers... Your end has come...", "Blue")
+                    task.wait(math.random(2, 3))
+                    EntityInfo.DeathHint(entityName .. " fades away... Farewell...", "Blue")
+                    break
+                end
+            end
+        end
     end
-end)()            
+end)()
 local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 
 coroutine.wrap(function()
     while true do
         task.wait(135)
 
-        applyTween(Color3.fromRGB(255, 0, 0), 1.5)
-        local Idle = GetGitSound("https://github.com/Brololto/G95-MOVING/blob/main/Screen_Recording_20230323-172501_YouTube%20(online-audio-converter.com)%20(1).mp3?raw=true", "anxddg")
-        Idle.Parent = workspace
-        Idle.Volume = 20
-        Idle:Play()
+        local latestRoom = game.ReplicatedStorage.GameData.LatestRoom.Value
+        local seekMovingExists = false
 
-        local entity = spawner.Create({
-            Entity = {
-                Name = "Obsession",
-                Asset = "https://github.com/Brololto/ExtremeModeG-95/blob/main/G-95Remastered-1.rbxm?raw=true",
-                HeightOffset = 0
-            },
-            Lights = {
-                Flicker = { Enabled = true, Duration = 1 },
-                Shatter = true,
-                Repair = false
-            },
-            Earthquake = { Enabled = true },
-            CameraShake = {
-                Enabled = true,
-                Range = 100,
-                Values = {1.5, 20, 0.1, 1}
-            },
-            Movement = {
-                Speed = 700,
-                Delay = 12,
-                Reversed = false
-            },
-            Damage = {
-                Enabled = true,
-                Range = 100,
-                Amount = 125
-            },
-            Crucifixion = {
-                Enabled = true,
-                Range = 40,
-                Resist = false,
-                Break = true
-            },
-            Death = {
-                Type = "Curious",
-                Hints = {"Oh hi", "I'm light", ":))) I WILL KILL YOU", "Scream!!!"},
-                Cause = "G-95"
-            }
-        })
-
-        entity:SetCallback("OnSpawned", function()
-            print("Entity has spawned")
-        end)
-
-        entity:SetCallback("OnStartMoving", function()
-            print("Entity has started moving")
-        end)
-
-        entity:SetCallback("OnEnterRoom", function(room, firstTime)
-            local roomName = room.Name
-            print("Entity has entered room: " .. roomName .. (firstTime and " for the first time" or " again"))
-        end)
-
-        entity:SetCallback("OnLookAt", function(lineOfSight)
-            print(lineOfSight and "Player is looking at entity" or "Player view is obstructed by something")
-        end)
-
-        entity:SetCallback("OnRebounding", function(startOfRebound)
-            print(startOfRebound and "Entity has started rebounding" or "Entity has finished rebounding")
-        end)
-
-        entity:SetCallback("OnDespawning", function()
-            local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
-            local character = game.Players.LocalPlayer.Character
-            local humanoid = character and character:FindFirstChild("Humanoid")
-
-            if humanoid and humanoid.Health > 0 then
-                achievementGiver({
-                    Title = "Scream Demon of Hell",
-                    Desc = "You will die",
-                    Reason = "Encounter G-95",
-                    Image = "rbxassetid://3457898957"
-                })
-            else
-                print("Achievement not granted.")
+        for _, obj in ipairs(game.Workspace:GetChildren()) do
+            if string.find(obj.Name, "SeekMoving") then
+                seekMovingExists = true
+                break
             end
-        end)
+        end
 
-        entity:SetCallback("OnDespawned", function()
-            local Slam = GetGitSound("https://github.com/Brololto/FUCKYOUFACCIST/blob/main/Screen_Recording_20230407-114843_YouTube%20(online-audio-converter.com).mp3?raw=true", "Slamsaa")
-            Slam.Parent = workspace
-            Slam.Volume = 20
-            Slam:Play()
-        end)
+        if latestRoom == 50 or latestRoom == 100 or seekMovingExists then
+            return -- Stops execution if room is 50/100 or any object contains "SeekMoving" in its name
+        else
+            applyTween(Color3.fromRGB(255, 0, 0), 1.5)
+            local Idle = GetGitSound("https://github.com/Brololto/G95-MOVING/blob/main/Screen_Recording_20230323-172501_YouTube%20(online-audio-converter.com)%20(1).mp3?raw=true", "anxddg")
+            Idle.Parent = workspace
+            Idle.Volume = 20
+            Idle:Play()
 
-        entity:SetCallback("OnDamagePlayer", function(newHealth)
-            print(newHealth == 0 and "Entity has killed the player" or "Entity has damaged the player")
-        end)
+            local entity = spawner.Create({
+                Entity = {
+                    Name = "Obsession",
+                    Asset = "https://github.com/Brololto/ExtremeModeG-95/blob/main/G-95Remastered-1.rbxm?raw=true",
+                    HeightOffset = 0
+                },
+                Lights = {
+                    Flicker = { Enabled = true, Duration = 1 },
+                    Shatter = true,
+                    Repair = false
+                },
+                Earthquake = { Enabled = true },
+                CameraShake = {
+                    Enabled = true,
+                    Range = 100,
+                    Values = {1.5, 20, 0.1, 1}
+                },
+                Movement = {
+                    Speed = 700,
+                    Delay = 12,
+                    Reversed = false
+                },
+                Damage = {
+                    Enabled = true,
+                    Range = 100,
+                    Amount = 125
+                },
+                Crucifixion = {
+                    Enabled = true,
+                    Range = 40,
+                    Resist = false,
+                    Break = true
+                },
+                Death = {
+                    Type = "Curious",
+                    Hints = {"Oh hi", "I'm light", ":))) I WILL KILL YOU", "Scream!!!"},
+                    Cause = "G-95"
+                }
+            })
 
-        entity:Run()
+            entity:SetCallback("OnSpawned", function()
+                print("Entity has spawned")
+            end)
+
+            entity:SetCallback("OnStartMoving", function()
+                print("Entity has started moving")
+            end)
+
+            entity:SetCallback("OnEnterRoom", function(room, firstTime)
+                local roomName = room.Name
+                print("Entity has entered room: " .. roomName .. (firstTime and " for the first time" or " again"))
+            end)
+
+            entity:SetCallback("OnLookAt", function(lineOfSight)
+                print(lineOfSight and "Player is looking at entity" or "Player view is obstructed by something")
+            end)
+
+            entity:SetCallback("OnRebounding", function(startOfRebound)
+                print(startOfRebound and "Entity has started rebounding" or "Entity has finished rebounding")
+            end)
+
+            entity:SetCallback("OnDespawning", function()
+                local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+                local character = game.Players.LocalPlayer.Character
+                local humanoid = character and character:FindFirstChild("Humanoid")
+
+                if humanoid and humanoid.Health > 0 then
+                    achievementGiver({
+                        Title = "Scream Demon of Hell",
+                        Desc = "You will die",
+                        Reason = "Encounter G-95",
+                        Image = "rbxassetid://3457898957"
+                    })
+                else
+                    print("Achievement not granted.")
+                end
+            end)
+
+            entity:SetCallback("OnDespawned", function()
+                local Slam = GetGitSound("https://github.com/Brololto/FUCKYOUFACCIST/blob/main/Screen_Recording_20230407-114843_YouTube%20(online-audio-converter.com).mp3?raw=true", "Slamsaa")
+                Slam.Parent = workspace
+                Slam.Volume = 20
+                Slam:Play()
+            end)
+
+            entity:SetCallback("OnDamagePlayer", function(newHealth)
+                print(newHealth == 0 and "Entity has killed the player" or "Entity has damaged the player")
+            end)
+
+            entity:Run()
+        end
     end
 end)()
